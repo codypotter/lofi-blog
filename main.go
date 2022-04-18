f990545cd79294a4de84f1f19c9d0d0dab33dbb4
Cody Potter <cody.potter@gearbox.com>
2022-04-18 10:51:35 -0700

    added go backend and basic routing

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"path"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.NoRoute(func(c *gin.Context) {
+		dir, file := path.Split(c.Request.RequestURI)
+		ext := filepath.Ext(file)
+		if file == "" || ext == "" {
+			c.File("./dist/lofi-blog/index.html")
+		} else {
+			c.File("./dist/lofi-blog/" + path.Join(dir, file))
+		}
+	})
+
+	api := r.Group("/api")
+	{
+		api.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+
+	if err := r.Run(); err != nil {
+		log.Fatal("Server failed to start")
+	}
+}
